5e70a26f16132d4e45699dc4fbc8ac840bc5cfbb
vimes1984 <churchill.c.j@gmail.com>
2015-01-15 18:10:56 +0100

    updating readme

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1>Welcome to our provisioned Wordpress install!</h1>
+<p>This is a basic danceenergy install it's missing the wp-config and the .sql to import for security reasons</p>
+
+<h3>Prerequisites</h3>
+<p>you are going to need at the very least:</p>
+<a href="https://www.virtualbox.org/">Virtualbox</a>
+<a href="https://www.vagrantup.com/downloads.html">Vagrant</a>
+
+<h3>Installation</h3>
+<p>After you have installed the above open up your Terminal and CD into a empty directory:</p>
+<pre>
+	<code>
+		cd /PATH/TO/DIRECTORY
+	</code>
+</pre>
+<p>Next grab the repo from git hub like so: </p>
+<code>
+	git clone git@github.com:accruemarketing/DanceEnergy.git
+</code>
+
+<p>Then you are going to need to place the provided .sql file into the following directory (the directory will not exist you will have to create it):</p>
+<code>
+   /PATH/TO/LOCAL/REPOE/provisioning/mysql/
+</code>
+<p>Place the provided <code>wp-config.php</code> file in the root of your repo.</p>
+<p>Back to your command line  you now need to run the command <code>vagrant up</code> then <code>vagrant provision</code></p>
+<p>after this we are done!! you should be able to go to http://localhost.localdomain:8080/ in your broswer and you should see our new local dancenery site!</p>
--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-yum -y install mysql-server httpd php-fpm php-mysql php-pgsql php-devel php-pear gcc gcc-c++ autoconf automake unzip wget nano
+yum -y install mysql-server httpd php-fpm php-mysql php-pgsql php-devel php-pear gcc gcc-c++ autoconf automake unzip wget nano at php-xmlwriter
 
 
 sed -i 's/html_errors = Off/html_errors = On/g' /etc/php.ini
@@ -19,12 +19,14 @@ sed -i 's/#EnableSendfile off/EnableSendfile off/g' /etc/httpd/conf/httpd.conf
 service iptables stop
 chkconfig iptables off
 
-mysql -uroot -e "set global net_buffer_length=1000000; set global max_allowed_packet=1000000000;"
+mysql -u root -e "set global net_buffer_length=1000000; set global max_allowed_packet=1000000000;"
 
-MYSQL_RESULT=`mysqlshow --user=root db7199_wrdp | grep -v Wildcard | grep -o db7199_wrdp`
+MYSQL_RESULT=`mysqlshow --user=root  db7199_wrdp | grep -v Wildcard | grep -o db7199_wrdp`
 
 if [ "$MYSQL_RESULT" != "db7199_wrdp" ]; then
-    echo "create database db7199_wrdp" | mysql -u root
+    echo "create database db7199_wrdp" | mysql -u root 
 fi
 
 mysql -u root db7199_wrdp < /vagrant/provisioning/mysql/db7199_wrdp.sql
+#Set local new urls
+mysql -u root -e "use db7199_wrdp; UPDATE wp_options SET option_value = replace(option_value, 'http://dancenergy.zenutech.com', 'http://localhost.localdomain:8080'); UPDATE wp_posts SET guid = replace(guid, 'http://dancenergy.zenutech.com','http://localhost.localdomain:8080'); UPDATE wp_posts SET post_content = replace(post_content, 'http://dancenergy.zenutech.com', 'http://localhost.localdomain:8080'); UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://dancenergy.zenutech.com', 'http://localhost.localdomain:8080'); UPDATE wp_options SET option_value = replace(option_value, '/dancenergy.zenutech.com/', '/localhost.localdomain:8080/');"
